bcda9cce75176bf5249102f7697892f2f7e9fced
skyripley <ripleyskylar@gmail.com>
2018-10-01 12:56:01 -0700

    Fixing classdeclsimple var list handling in pretty print

--- src/AST/Visitor/PrettyPrintVisitor.java
+++ src/AST/Visitor/PrettyPrintVisitor.java
@@ -2,39 +2,67 @@ package AST.Visitor;
 
 import AST.*;
 
+
 // Sample print visitor from MiniJava web site.
 
 public class PrettyPrintVisitor implements Visitor {
 
-  // Display added for toy example language.  Not used in regular MiniJava
+  int tabs = 0;
+
+  public void incTab() {
+    tabs += 1;
+  }
+
+  public void decTab() {
+    tabs -= 1;
+    if (tabs < 0)
+      tabs = 0;
+  }
+
+  public void printTab() {
+    String spaces = "";
+    for (int i = 0; i < tabs * 4; i++)
+      spaces += " ";
+    System.out.print(spaces);
+  }
+
+  // Display added for toy example language. Not used in regular MiniJava
   public void visit(Display n) {
+    printTab();
     System.out.print("display ");
     n.e.accept(this);
-    System.out.print(";");
+    System.out.println(";");
   }
-  
+
   // MainClass m;
   // ClassDeclList cl;
   public void visit(Program n) {
     n.m.accept(this);
-    for ( int i = 0; i < n.cl.size(); i++ ) {
-        System.out.println();
-        n.cl.get(i).accept(this);
+    for (int i = 0; i < n.cl.size(); i++) {
+      System.out.println();
+      n.cl.get(i).accept(this);
     }
   }
-  
+
   // Identifier i1,i2;
   // Statement s;
   public void visit(MainClass n) {
+    printTab();
     System.out.print("class ");
     n.i1.accept(this);
     System.out.println(" {");
-    System.out.print("  public static void main (String [] ");
+    incTab();
+    printTab();
+    System.out.print("public static void main (String [] ");
     n.i2.accept(this);
     System.out.println(") {");
-    System.out.print("    ");
+    incTab();
     n.s.accept(this);
-    System.out.println("  }");
+    decTab();
+    printTab();
+    System.out.println("}");
+    decTab();
+    printTab();
     System.out.println("}");
   }
 
@@ -42,22 +70,25 @@ public class PrettyPrintVisitor implements Visitor {
   // VarDeclList vl;
   // MethodDeclList ml;
   public void visit(ClassDeclSimple n) {
+    printTab();
     System.out.print("class ");
     n.i.accept(this);
     System.out.println(" { ");
-    for ( int i = 0; i < n.vl.size(); i++ ) {
-        System.out.print("  ");
+    incTab();
+    if (n.vl != null) {
+      for (int i = 0; i < n.vl.size(); i++) {
         n.vl.get(i).accept(this);
-        if ( i+1 < n.vl.size() ) { System.out.println(); }
+      }
     }
-    for ( int i = 0; i < n.ml.size(); i++ ) {
-        System.out.println();
-        n.ml.get(i).accept(this);
+    for (int i = 0; i < n.ml.size(); i++) {
+      n.ml.get(i).accept(this);
     }
+    decTab();
     System.out.println();
+    printTab();
     System.out.println("}");
   }
- 
+
   // Identifier i;
   // Identifier j;
   // VarDeclList vl;
@@ -68,26 +99,28 @@ public class PrettyPrintVisitor implements Visitor {
     System.out.println(" extends ");
     n.j.accept(this);
     System.out.println(" { ");
-    for ( int i = 0; i < n.vl.size(); i++ ) {
-        System.out.print("  ");
-        n.vl.get(i).accept(this);
-        if ( i+1 < n.vl.size() ) { System.out.println(); }
-    }
-    for ( int i = 0; i < n.ml.size(); i++ ) {
-        System.out.println();
-        n.ml.get(i).accept(this);
+    incTab();
+    for (int i = 0; i < n.vl.size(); i++) {
+      n.vl.get(i).accept(this);
     }
     System.out.println();
+    for (int i = 0; i < n.ml.size(); i++) {
+      n.ml.get(i).accept(this);
+    }
+    decTab();
+    System.out.println();
+    printTab();
     System.out.println("}");
   }
 
   // Type t;
   // Identifier i;
   public void visit(VarDecl n) {
+    printTab();
     n.t.accept(this);
     System.out.print(" ");
     n.i.accept(this);
-    System.out.print(";");
+    System.out.println(";");
   }
 
   // Type t;
@@ -97,30 +130,36 @@ public class PrettyPrintVisitor implements Visitor {
   // StatementList sl;
   // Exp e;
   public void visit(MethodDecl n) {
-    System.out.print("  public ");
+    System.out.println();
+    printTab();
+    System.out.print("public ");
     n.t.accept(this);
     System.out.print(" ");
     n.i.accept(this);
     System.out.print(" (");
-    for ( int i = 0; i < n.fl.size(); i++ ) {
-        n.fl.get(i).accept(this);
-        if (i+1 < n.fl.size()) { System.out.print(", "); }
+    for (int i = 0; i < n.fl.size(); i++) {
+      n.fl.get(i).accept(this);
+      if (i + 1 < n.fl.size()) {
+        System.out.print(", ");
+      }
     }
     System.out.println(") { ");
-    for ( int i = 0; i < n.vl.size(); i++ ) {
-        System.out.print("    ");
-        n.vl.get(i).accept(this);
-        System.out.println("");
+    incTab();
+    for (int i = 0; i < n.vl.size(); i++) {
+      n.vl.get(i).accept(this);
     }
-    for ( int i = 0; i < n.sl.size(); i++ ) {
-        System.out.print("    ");
-        n.sl.get(i).accept(this);
-        if ( i < n.sl.size() ) { System.out.println(""); }
+    System.out.println();
+    for (int i = 0; i < n.sl.size(); i++) {
+      n.sl.get(i).accept(this);
     }
-    System.out.print("    return ");
+    System.out.println();
+    printTab();
+    System.out.print("return ");
     n.e.accept(this);
     System.out.println(";");
-    System.out.print("  }");
+    decTab();
+    printTab();
+    System.out.println("}");
   }
 
   // Type t;
@@ -151,30 +190,46 @@ public class PrettyPrintVisitor implements Visitor {
   // StatementList sl;
   public void visit(Block n) {
     System.out.println("{ ");
-    for ( int i = 0; i < n.sl.size(); i++ ) {
-        System.out.print("      ");
-        n.sl.get(i).accept(this);
-        System.out.println();
+    incTab();
+    for (int i = 0; i < n.sl.size(); i++) {
+      n.sl.get(i).accept(this);
     }
-    System.out.print("    } ");
+    decTab();
+    printTab();
+    System.out.println("}");
   }
 
   // Exp e;
   // Statement s1,s2;
   public void visit(If n) {
+    printTab();
     System.out.print("if (");
     n.e.accept(this);
-    System.out.println(") ");
-    System.out.print("    ");
+    System.out.print(") ");
+    if ((n.s1 instanceof Block) == false) {
+      System.out.println();
+      incTab();
+    }
     n.s1.accept(this);
-    System.out.println();
-    System.out.print("    else ");
+    if ((n.s1 instanceof Block) == false) {
+      decTab();
+    }
+    printTab();
+    System.out.print("else ");
+    if ((n.s2 instanceof Block) == false) {
+      System.out.println();
+      incTab();
+    }
     n.s2.accept(this);
+    if ((n.s2 instanceof Block) == false) {
+      decTab();
+    }
   }
 
   // Exp e;
   // Statement s;
   public void visit(While n) {
+    printTab();
     System.out.print("while (");
     n.e.accept(this);
     System.out.print(") ");
@@ -183,29 +238,32 @@ public class PrettyPrintVisitor implements Visitor {
 
   // Exp e;
   public void visit(Print n) {
+    printTab();
     System.out.print("System.out.println(");
     n.e.accept(this);
-    System.out.print(");");
+    System.out.println(");");
   }
-  
+
   // Identifier i;
   // Exp e;
   public void visit(Assign n) {
+    printTab();
     n.i.accept(this);
     System.out.print(" = ");
     n.e.accept(this);
-    System.out.print(";");
+    System.out.println(";");
   }
 
   // Identifier i;
   // Exp e1,e2;
   public void visit(ArrayAssign n) {
+    printTab();
     n.i.accept(this);
     System.out.print("[");
     n.e1.accept(this);
     System.out.print("] = ");
     n.e2.accept(this);
-    System.out.print(";");
+    System.out.println(";");
   }
 
   // Exp e1,e2;
@@ -275,9 +333,11 @@ public class PrettyPrintVisitor implements Visitor {
     System.out.print(".");
     n.i.accept(this);
     System.out.print("(");
-    for ( int i = 0; i < n.el.size(); i++ ) {
-        n.el.get(i).accept(this);
-        if ( i+1 < n.el.size() ) { System.out.print(", "); }
+    for (int i = 0; i < n.el.size(); i++) {
+      n.el.get(i).accept(this);
+      if (i + 1 < n.el.size()) {
+        System.out.print(", ");
+      }
     }
     System.out.print(")");
   }
