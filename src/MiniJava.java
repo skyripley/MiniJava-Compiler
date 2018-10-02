d7bbf7f857f27403428da349085591e3806e6698
skyripley <ripleyskylar@gmail.com>
2018-10-02 14:36:27 -0700

    Modifies message when errors are detetced

--- src/MiniJava.java
+++ src/MiniJava.java
@@ -21,9 +21,16 @@ public class MiniJava {
             parser parser = new parser(scanner, complexSymbolFactory);
             Symbol root;
             root = parser.parse();
-            Program program = (Program) root.value;
-            PrettyPrintVisitor prettyPrintVisitor = new PrettyPrintVisitor();
-            prettyPrintVisitor.visit(program);
+            if (parser.errorDetected) {
+                System.out.println("\nParsing complete, but syntax errors were detected");
+                return_code = 1;
+            }
+            else {
+                System.out.println("Parsing complete - no errors found");
+                Program program = (Program) root.value;
+                PrettyPrintVisitor prettyPrintVisitor = new PrettyPrintVisitor();
+                prettyPrintVisitor.visit(program);
+            }
             return return_code;
         } catch (Exception exception) {
             System.err.println("Unexpected internal compiler error: " + exception.toString());
